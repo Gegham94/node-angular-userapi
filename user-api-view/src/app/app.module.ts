8b1a316a9118e1c153f708f19419503a0fcfd93f
hayk957 <nazaryanhayk4@gmail>
2021-09-09 11:22:13 +0400

    add login register

--- user-api-view/src/app/app.module.ts
+++ user-api-view/src/app/app.module.ts
@@ -1,11 +1,13 @@
 import { NgModule, CUSTOM_ELEMENTS_SCHEMA } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { AngularMaterialModule } from './angular-material.module';
+import { FlexLayoutModule } from '@angular/flex-layout';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 
-import { AppComponent } from './app.component';
-import { LoginComponent } from './login/login.component';
-import { RegisterComponent } from './register/register.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { AppComponent } from './app.component';
+import { LoginComponent } from './components/login/login.component';
+import { RegisterComponent } from './components/register/register.component';
 
 @NgModule({
   declarations: [
@@ -16,7 +18,9 @@ import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
   imports: [
     BrowserModule,
     BrowserAnimationsModule,
-    AngularMaterialModule
+    AngularMaterialModule,
+    [FlexLayoutModule ],
+    [FormsModule, ReactiveFormsModule]
   ],
   providers: [],
   bootstrap: [AppComponent],
--- user_api/lib/redisConnect.js
+++ user_api/lib/redisConnect.js
@@ -1,6 +1,8 @@
 const Promise = require("bluebird");
 const redis = Promise.promisifyAll(require("redis"));
 
-const client = redis.createClient();
+let client = redis.createClient({ 
+    host: '127.0.0.1', port: 6379 
+  });
 
 module.exports = client;
